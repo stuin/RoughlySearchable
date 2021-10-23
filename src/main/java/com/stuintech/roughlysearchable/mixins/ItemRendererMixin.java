023d2510f1b4c253ea22fd4648c66075cc368099
Stuart Irwin <stuin01@gmail.com>
2021-10-23 11:42:59 -0700

    - Updated rei version

--- src/main/java/com/stuintech/roughlysearchable/mixins/ItemRendererMixin.java
+++ src/main/java/com/stuintech/roughlysearchable/mixins/ItemRendererMixin.java
@@ -1,6 +1,5 @@
 package com.stuintech.roughlysearchable.mixins;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.stuintech.roughlysearchable.api.ICustomConfig;
 import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.api.common.entry.type.VanillaEntryTypes;
@@ -9,8 +8,10 @@ import me.shedaniel.rei.impl.client.gui.ScreenOverlayImpl;
 import me.shedaniel.rei.impl.client.gui.widget.EntryListWidget;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.resource.SynchronousResourceReloader;
@@ -21,19 +22,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin implements SynchronousResourceReloader {
-    @Inject(at = @At("NEW"), method = "renderGuiItemModel(Lnet/minecraft/item/ItemStack;IILnet/minecraft/client/render/model/BakedModel;)V")
-    protected void renderGuiItemModel(ItemStack stack, int x, int y, BakedModel model, CallbackInfo ci) {
-        EntryListWidget widget = ScreenOverlayImpl.getEntryListWidget();
-        if(ICustomConfig.getConfig().RS_search && MinecraftClient.getInstance().currentScreen instanceof HandledScreen &&
-                !REIRuntimeImpl.getSearchField().getText().isEmpty()) {
+    boolean shouldSearch() {
+        return ICustomConfig.getConfig().RS_search && MinecraftClient.getInstance().currentScreen instanceof HandledScreen &&
+                !REIRuntimeImpl.getSearchField().getText().isEmpty();
+    }
+
+    @Inject(at = @At("HEAD"), method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V")
+    public void renderItem(ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider
+        vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+        if(!stack.isEmpty() && shouldSearch()) {
+            EntryListWidget widget = ScreenOverlayImpl.getEntryListWidget();
             float grow = ICustomConfig.getIndicator().RS_trueSize;
             float shrink = ICustomConfig.getIndicator().RS_falseSize;
-            MatrixStack matrixStack = RenderSystem.getModelViewStack();
+
+            matrices.push();
             if(widget.matches(EntryStack.of(VanillaEntryTypes.ITEM, stack)))
-                matrixStack.scale(grow, grow, grow);
+                matrices.scale(grow, grow, grow);
             else
-                matrixStack.scale(shrink, shrink, shrink);
-            RenderSystem.applyModelViewMatrix();
+                matrices.scale(shrink, shrink, shrink);
+        }
+    }
+
+    @Inject(at = @At("RETURN"), method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V")
+    public void renderItemEnd(ItemStack stack, ModelTransformation.Mode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider
+            vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+        if(!stack.isEmpty() && shouldSearch()) {
+            matrices.pop();
         }
     }
 }
